757072acf4acdc58eedb12fa9cebff9b5999cdaf
gilang_gans <gilangsos99@gmail.com>
2022-04-09 20:12:27 +0700

    Tugas javascript 2
    Tugas ini untuk niomic

---
+++ javascript/tugas2.js
@@ -0,0 +1,32 @@
+// javascript number
+var angka = 30;
+
+console.log(angka);
+
+// javascript string
+var huruf = "membaca buku";
+
+console.log(huruf);
+
+// javascript boolean
+var ojon = "false";
+shinta = "800";
+
+if (shinta>100)
+ojon = "true"
+else
+ojon = "false";
+
+console.log(ojon);
+
+// javascript null atau undifined
+var  kosong = null;
+var  undifined;
+console.log(kosong);
+console.log(undifined);
+
+// javascript array
+var angka1 = [3,4,5,6,7,8];
+
+console.log(angka1);
+console.log(angka1[4]);
